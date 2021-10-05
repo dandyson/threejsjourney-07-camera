cebe5109570bd0778004b3582501af843df42e7e
dandyson <dannydyson297@gmail.com>
2021-10-05 21:01:19 +0100

    Initial Commit

---
+++ src/script.js
@@ -0,0 +1,86 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 800
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 
+//     1 * aspectRatio, 
+//     1, 
+//     -1, 
+//     0.1, 
+//     100
+// )
+// camera.position.x = 2
+// camera.position.y = 2
+
+
+// Cursor
+
+const cursor = {
+    x: 0,
+    y: 0
+}
+
+window.addEventListener("mousemove", (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+
+    console.log(cursor.x, cursor.y)
+})
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Animate
+const clock = new THREE.Clock()
+
+// Controls
+const controls = new OrbitControls(camera, canvas, renderer.autoRotate, renderer.autoRotateSpeed)
+// controls.target.y = 1
+controls.enableDamping = true
+
+camera.position.z = 3
+scene.add(camera)
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
